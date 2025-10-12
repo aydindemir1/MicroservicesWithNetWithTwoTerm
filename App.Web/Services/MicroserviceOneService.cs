bedc052d76f743fd393d69999dba53a5c0a92af2
aydin <aydin@AYDIN>
2025-10-13 00:55:00 +0300

    3.hafta 5.gun tamamlandi

---
+++ App.Web/Services/MicroserviceOneService.cs
@@ -0,0 +1,65 @@
+﻿using Duende.IdentityModel.Client;
+
+namespace App.Web.Services
+{
+
+    public class MicroserviceOneService(HttpClient client, IConfiguration configuration)
+    {
+
+        public async Task<ExchangeResponse> GetExchange()
+        {
+
+            var clientId = configuration.GetSection("Client")["Id"]!;
+
+            var clientSecret = configuration.GetSection("Client")["Secret"]!;
+
+            var Authority = configuration.GetSection("Client")["Authority"]!;
+
+            var discoveryResult = await  client.GetDiscoveryDocumentAsync($"{Authority}/.well-known/openid-configuration");
+
+
+            if (discoveryResult.IsError)
+            {
+                // logging
+                // reuturn model
+            }
+
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryResult.TokenEndpoint,
+                ClientId = clientId,
+                ClientSecret = clientSecret
+               
+            });
+
+            if (tokenResponse.IsError)
+            {
+                // logging
+                // reuturn model
+            }
+
+            
+
+
+
+            client.SetBearerToken(tokenResponse.AccessToken!);
+
+
+
+            var response = await client.GetAsync("/exchange");
+
+            if (response.IsSuccessStatusCode)
+            {
+            }
+
+
+            var responseContent = await  response.Content.ReadFromJsonAsync<ExchangeResponse>();
+
+            return responseContent!;
+
+        }
+    }
+    
+
+}
